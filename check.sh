53e21da1153359e79281ce218e0e986b2a2e3ba5
Milos Vasic <milos85vasic@gmail.com>
2020-09-11 16:51:22 +0200

    MSF-248 - Increased timeout.

--- check.sh
+++ check.sh
@@ -4,7 +4,7 @@ ELAPSED=0
 TIMEOUT=120
 CONTAINER=$1
 until [ $ELAPSED -eq "${TIMEOUT}" ] || docker ps -a --filter "status=running" --filter "name=${CONTAINER}" | grep "${CONTAINER}"; do
-  echo "Waiting for container to start: ${CONTAINER}"
+  echo "Waiting for container to start: ${CONTAINER}, retry: $ELAPSED"
   sleep $((ELAPSED = ELAPSED + 1))
 done
 if test "$ELAPSED" -eq ${TIMEOUT}
