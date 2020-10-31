1a5727310ae1c2e5d37ba2fcd0023dbad9191029
Milos Vasic <milos85vasic@gmail.com>
2020-10-31 22:49:49 +0100

    MSF-28 - Qemu: grab cursor.

--- Qemu/create_machine.sh
+++ Qemu/create_machine.sh
@@ -5,5 +5,7 @@ machine=$1
 acceleration=$(sh get_acceleration.sh)
 
 sh create_disk.sh "$machine" 20 &&
-  qemu-system-x86_64 -accel "$acceleration" -cpu host -m 2048 -smp 2 -cdrom "$iso" \
-    -vga virtio -drive file="$machine.qcow2,format=qcow2,if=virtio"
+  qemu-system-x86_64 -accel "$acceleration" -cpu host -m 2048 -smp 2 \
+  -show-cursor -usb -device usb-tablet -vga virtio \
+  -drive file="$machine.qcow2,format=qcow2,if=virtio" \
+  -cdrom "$iso"
--- Qemu/run_machine.sh
+++ Qemu/run_machine.sh
@@ -4,4 +4,5 @@ machine=$1
 acceleration=$(sh get_acceleration.sh)
 
 qemu-system-x86_64 -accel "$acceleration" -cpu host -m 2048 -smp 2 \
-    -vga virtio -drive file="$machine.qcow2,format=qcow2,if=virtio"
+  -show-cursor -usb -device usb-tablet -vga virtio \
+  -drive file="$machine.qcow2,format=qcow2,if=virtio"
