679e7a6997d735f6bb43f08d531a8fccf7b964e8
Milos Vasic <milos85vasic@gmail.com>
2020-10-31 22:15:41 +0100

    MSF-28 - Working on Qemu scripts.

--- Qemu/run_machine.sh
+++ Qemu/run_machine.sh
@@ -2,5 +2,5 @@
 
 machine=$1
 
-qemu-system-x86_64 -cpu host -m 2048 -smp 2 \
+qemu-system-x86_64 -accel hvf -cpu host -m 2048 -smp 2 \
  -vga virtio -drive file="$machine.qcow2,format=qcow2,if=virtio"
