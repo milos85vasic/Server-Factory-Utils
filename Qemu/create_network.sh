8ea670993926164f975bc6dc38d0605b06274bb5
Milos Vasic <milos85vasic@gmail.com>
2020-11-01 14:36:46 +0100

    MSF-28 - Qemu: creating network.

--- Qemu/create_network.sh
+++ Qemu/create_network.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-if ifconfig server_factory_network_bridge | grep -i "does not exist"; then
+bridgeName="server_factory_network_bridge"
 
-  echo "Creating network bridge: server_factory_network_bridge"
+if ! (ifconfig server_factory_network_bridge); then
+
+  echo "$bridgeName: Network bridge is not yet available"
+  echo "$bridgeName: Creating network bridge"
 else
 
-  echo "Network bridge is available: server_factory_network_bridge"
+  echo "$bridgeName: Network bridge is available"
 fi
