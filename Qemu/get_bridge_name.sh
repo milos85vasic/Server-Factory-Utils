41bd30d686205c3a1f25e82632c9bc2ecd14a9f1
Milos Vasic <milos85vasic@gmail.com>
2020-11-01 15:45:05 +0100

    MSF-28 - Qemu: creating network.

--- Qemu/get_bridge_name.sh
+++ Qemu/get_bridge_name.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-for ITER in 1 .. 100
-do
-  bridge="bridge$ITER"
-  if ! (ifconfig "$bridge" > /dev/null); then
+script_path="/usr/local/bin"
+script_path_full="$script_path/get_server_factory_bridge_name.sh"
 
-    echo "$bridge"
-    exit 0
-  fi
-done
+if test -e "$script_path_full"; then
+
+  sh "$script_path_full"
+else
+
+  for ITER in 1 .. 100
+  do
+    bridge="bridge$ITER"
+    if ! (ifconfig "$bridge" > /dev/null); then
+
+      echo """
+      #!/bin/sh
+
+      echo $bridge
+      """ > "$script_path_full" && chmod 750 "$script_path_full"
+      sh "$script_path_full"
+      exit 0
+    fi
+  done
+fi
 
 echo "ERROR: Could not obtain bridge name"
 exit 1
