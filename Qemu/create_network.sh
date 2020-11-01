e5e26b80790e42896f5178ecea14312159e0f557
Milos Vasic <milos85vasic@gmail.com>
2020-11-01 17:52:54 +0100

    MSF-28 - Qemu: creating network.

--- Qemu/create_network.sh
+++ Qemu/create_network.sh
@@ -1,28 +1,5 @@
 #!/bin/sh
 
-if ! test -e /usr/local/bin/ip; then
-
-  echo "'ip' command not found on the system"
-  if uname | grep -i "darwin" > /dev/null; then
-
-    echo "'ip' command will be installed"
-    if curl --remote-name -L https://github.com/brona/iproute2mac/raw/master/src/ip.py && \
-      chmod +x ip.py && \
-      mv ip.py /usr/local/bin/ip; then
-
-        echo "'ip' command installed"
-    else
-
-      echo "'ip' command installation failed"
-      exit 1
-    fi
-  else
-
-    echo "ERROR: 'ip' command is required to continue, please install it for your system"
-    exit 1
-  fi
-fi
-
 bindBridgeTo="en0"
 bridgeName=$(sh create_and_get_bridge.sh)
 
@@ -34,7 +11,7 @@ if ! (ifconfig "$bridgeName"); then
     echo "Step: Bridge created" && \
     sudo ifconfig "$bridgeName" addm "$bindBridgeTo" && \
     echo "Step: Bridge bound to: $bindBridgeTo" && \
-    sudo ifconfig bridge0 up && \
+    sudo ifconfig "$bridgeName" up && \
     echo "Step: Bridge is up"; then
 
       echo "$bridgeName: Network bridge created"
--- Qemu/machine.sh
+++ Qemu/machine.sh
@@ -5,8 +5,11 @@ machine=$1
 display=$(sh get_display.sh)
 acceleration=$(sh get_acceleration.sh)
 disk=$(sh create_disk.sh "$machine" 20)
+#tapName=$(sh create_and_get_tap.sh)
+#bridgeName=$(sh create_and_get_bridge.sh)
 
-sh create_network.sh &&
+sh create_network.sh && \
+#  ip link set "$tapName" master "$bridgeName"
   qemu-system-x86_64 -accel "$acceleration" -cpu host -m 2048 -smp 2 \
     -display "$display",show-cursor=on -usb -device usb-tablet -vga virtio \
     -drive file="$disk,format=qcow2,if=virtio" \
