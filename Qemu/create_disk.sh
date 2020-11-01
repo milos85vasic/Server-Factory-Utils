125ab9eb4886f369c4d3bcb09e4fef5ff6d52ffa
Milos Vasic <milos85vasic@gmail.com>
2020-11-01 16:49:00 +0100

    MSF-28 - Qemu: creating network.

--- Qemu/create_disk.sh
+++ Qemu/create_disk.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-disk=$1
 size=$2
+diskPath=$1
+disk="$diskPath/disk.qcow2"
 
-if ! test -e "$disk.qcow2"; then
+if ! test -e "$disk"; then
 
-  qemu-img create -f qcow2 "$disk.qcow2" "${size}G"
-else
+  mkdir -p "$diskPath"
+  if ! qemu-img create -f qcow2 "$disk" "${size}G" >> /dev/null; then
 
-  echo "$disk.qcow2 already exist"
+    echo "$disk was not created"
+    exit 1
+  fi
 fi
+
+echo "$disk"
--- Qemu/machine.sh
+++ Qemu/machine.sh
@@ -4,10 +4,10 @@ iso=$2
 machine=$1
 display=$(sh get_display.sh)
 acceleration=$(sh get_acceleration.sh)
+disk=$(sh create_disk.sh "$machine" 20)
 
-sh create_disk.sh "$machine" 20 &&
+sh create_network.sh &&
   qemu-system-x86_64 -accel "$acceleration" -cpu host -m 2048 -smp 2 \
-  -display "$display",show-cursor=on -usb -device usb-tablet -vga virtio \
-  -drive file="$machine.qcow2,format=qcow2,if=virtio" \
-  -cdrom "$iso"
-
+    -display "$display",show-cursor=on -usb -device usb-tablet -vga virtio \
+    -drive file="$disk,format=qcow2,if=virtio" \
+    -cdrom "$iso"
