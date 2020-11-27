975dfe4e413e2d18d7a10ab3d514fc4606be1673
Milos Vasic <milos85vasic@gmail.com>
2020-11-27 14:45:34 +0100

    MSF-337 - Generating SSH certificate.

--- init_ssh_access.sh
+++ init_ssh_access.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
 
+machine=$1
+
+certificate=~/.ssh/server_factory
+echo "$certificate: Checking certificate"
+if test -e "$certificate"; then
+
+  echo "$certificate: Available"
+else
+
+  echo "WARNING: $certificate not available, we will generate it"
+  if echo "$certificate" | ssh-keygen -t ed25519 -C "client@server.factory"; then
+
+    echo "$certificate: Generated"
+  else
+
+    echo "ERROR: $certificate not Generated"
+  fi
+fi
