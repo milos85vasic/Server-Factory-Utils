c55c76206ea6305e850ef91ba2c95d3e7c815717
Milos Vasic <milos85vasic@gmail.com>
2020-12-05 21:56:24 +0100

    MSF-364 - Get IP address script improved.

--- getip.sh
+++ getip.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
-ADDRESS=$1
-if nslookup "${ADDRESS}" | grep "can't find"
-then
+address=$1
+if nslookup "${address}" | grep "can't find" >/dev/null 2>&1; then
 
-  echo "Could not obtain ip address for: $ADDRESS"
+  ping_result=$(ping -c 3 "$address")
+  if echo "$ping_result" | grep "100.0% packet loss" >/dev/null 2>&1; then
+
+    echo "Could not obtain ip address for: $address"
+    exit 1
+  else
+
+    if echo "$ping_result" | grep "cannot resolve" >/dev/null 2>&1; then
+
+      echo "Could not obtain ip address for: $address"
+      exit 1
+    else
+
+      data_bytes="data bytes"
+      line=$(echo "$ping_result" | grep "$data_bytes")
+      line=$(echo "$line" | sed -e "s/PING $address \(.*\)$data_bytes/\1/" | tr -d '()' | cut -f1 -d":")
+      line=$(echo "$line" | tr -d "[:blank:]")
+      if [ "$line" != "" ]; then
+
+        echo "$line"
+      fi
+    fi
+  fi
 else
 
-  nslookup "${ADDRESS}" | sed '/^[[:space:]]*$/d' | tail -1 | tr -d "[:blank:]" | sed s/Address://
+  nslookup "${address}" | sed '/^[[:space:]]*$/d' | tail -1 | tr -d "[:blank:]" | sed s/Address://
 fi
