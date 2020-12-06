951e7fc6b396499b51038e81fb2f53fb5451fdde
Milos Vasic <milos85vasic@gmail.com>
2020-12-06 16:38:43 +0100

    MSF-364 - Get IP script improved.

--- getip.sh
+++ getip.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 address=$1
+error="Could not obtain ip address for: $address"
 if nslookup "${address}" | grep "can't find" >/dev/null 2>&1; then
 
   ping_result=$(ping -c 3 "$address")
   if echo "$ping_result" | grep "100.0% packet loss" >/dev/null 2>&1; then
 
-    echo "Could not obtain ip address for: $address"
+    echo "$error"
     exit 1
   else
 
     if echo "$ping_result" | grep "cannot resolve" >/dev/null 2>&1; then
 
-      echo "Could not obtain ip address for: $address"
+      echo "$error"
       exit 1
     else
 
@@ -28,5 +29,20 @@ if nslookup "${address}" | grep "can't find" >/dev/null 2>&1; then
   fi
 else
 
-  nslookup "${address}" | sed '/^[[:space:]]*$/d' | tail -1 | tr -d "[:blank:]" | sed s/Address://
+  addr="Address:"
+  lookup_data=$(nslookup "${address}" | sed '/^[[:space:]]*$/d' | grep "$addr")
+  addresses=$(echo "$lookup_data" | tr -d "[:blank:]")
+  addresses=$(echo "$addresses" | sed -e "s/^$addr//" | tr '\r\n' ';')
+  export IFS=";"
+  for candidate in $addresses; do
+
+    if expr "$candidate" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null 2>&1; then
+
+      echo "$candidate"
+      exit 0
+    fi
+  done
+
+  echo "$error"
+  exit 1
 fi
