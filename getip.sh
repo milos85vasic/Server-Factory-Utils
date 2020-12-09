2cc0d17c427d589a02fb43cd5db003d44a41b7c4
Milos Vasic <milos85vasic@gmail.com>
2020-12-09 22:34:02 +0100

    MSF-368 - Simplifying get IP scrip.

--- getip.sh
+++ getip.sh
@@ -3,35 +3,13 @@
 address=$1
 error="Could not obtain ip address for: $address"
 if ! which nslookup >/dev/null 2>&1 ||
-  nslookup "${address}" | grep "can't find" >/dev/null 2>&1; then
+  nslookup "$address" | grep "can't find" >/dev/null 2>&1; then
 
-  ping_result=$(ping -c 3 "$address")
-  if echo "$ping_result" | grep "100.0% packet loss" >/dev/null 2>&1; then
-
-    echo "$error"
-    exit 1
-  else
-
-    if echo "$ping_result" | grep "cannot resolve" >/dev/null 2>&1; then
-
-      echo "$error"
-      exit 1
-    else
-
-      data_bytes="data bytes"
-      line=$(echo "$ping_result" | grep "$data_bytes")
-      line=$(echo "$line" | sed -e "s/PING $address \(.*\)$data_bytes/\1/" | tr -d '()' | cut -f1 -d":")
-      line=$(echo "$line" | tr -d "[:blank:]")
-      if [ "$line" != "" ]; then
-
-        echo "$line"
-      fi
-    fi
-  fi
+  ping -c1 "$address" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
 else
 
   addr="Address:"
-  lookup_data=$(nslookup "${address}" | sed '/^[[:space:]]*$/d' | grep "$addr")
+  lookup_data=$(nslookup "$address" | sed '/^[[:space:]]*$/d' | grep "$addr")
   addresses=$(echo "$lookup_data" | tr -d "[:blank:]")
   addresses=$(echo "$addresses" | sed -e "s/^$addr//" | tr '\r\n' ';')
   export IFS=";"
