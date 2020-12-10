0556c76c39875c568b0952ad499e91526b10e257
Milos Vasic <milos85vasic@gmail.com>
2020-12-10 14:00:33 +0100

    MSF-368 - Simplifying get IP scrip.

--- getip.sh
+++ getip.sh
@@ -1,27 +1,3 @@
 #!/bin/sh
 
-address=$1
-error="Could not obtain ip address for: $address"
-if ! which nslookup >/dev/null 2>&1 ||
-  nslookup "$address" | grep "can't find" >/dev/null 2>&1; then
-
-  ping -c1 "$address" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
-else
-
-  addr="Address:"
-  lookup_data=$(nslookup "$address" | sed '/^[[:space:]]*$/d' | grep "$addr")
-  addresses=$(echo "$lookup_data" | tr -d "[:blank:]")
-  addresses=$(echo "$addresses" | sed -e "s/^$addr//" | tr '\r\n' ';')
-  export IFS=";"
-  for candidate in $addresses; do
-
-    if expr "$candidate" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null 2>&1; then
-
-      echo "$candidate"
-      exit 0
-    fi
-  done
-
-  echo "$error"
-  exit 1
-fi
+ping -c1 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
