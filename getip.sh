061b4648053b971e07b884ce907675b7ab5d12d8
Milos Vasic <milos85vasic@gmail.com>
2020-12-12 17:15:23 +0100

    MSF-372 - Improving get IP script.

--- getip.sh
+++ getip.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-ping -c1 -4 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
+if uname | grep -i "darwin" >/dev/null 2>&1; then
+
+  ping -c1 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
+else
+
+  ping -c1 -4 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
+fi
