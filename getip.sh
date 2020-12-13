2d5c3a99c4fd36d269bd009027a84cd94072c99a
Milos Vasic <milos85vasic@gmail.com>
2020-12-13 15:30:41 +0100

    MSF-372 - Force IPv4 on macOS host.

--- getip.sh
+++ getip.sh
@@ -2,7 +2,7 @@
 
 if uname | grep -i "darwin" >/dev/null 2>&1; then
 
-  ping -c 1 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
+  ping -c 1 -k 4 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
 else
 
   ping -c 1 -4 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
