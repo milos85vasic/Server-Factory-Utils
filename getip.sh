5cf6c73d92feb9c9373120787a5ecd935b12d9e6
Milos Vasic <milos85vasic@gmail.com>
2020-12-13 15:06:03 +0100

    MSF-372 - Commands priority.

--- getip.sh
+++ getip.sh
@@ -2,8 +2,8 @@
 
 if uname | grep -i "darwin" >/dev/null 2>&1; then
 
-  ping -c1 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
+  ping -c 1 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
 else
 
-  ping -c1 -4 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
+  ping -c 1 -4 "$1" | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'
 fi
