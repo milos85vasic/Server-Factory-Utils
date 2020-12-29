1c3be9f65a57da54f828bf56ce5b2334cae75366
Milos Vasic <milos85vasic@gmail.com>
2020-12-29 13:54:37 +0100

    MSF-233 - Monitor directory size.

--- monitor_directory_size.sh
+++ monitor_directory_size.sh
@@ -4,10 +4,10 @@ directory="$1"
 if test -e "$directory"; then
 
   size=$(sh get_directory_size.sh "$directory")
-  printf "\r                                 "
-  printf "%s, $size"
+  printf "%s" "$size"
   sleep 3
-  sh monitor_directory_size.sh "$directory"
+  printf "\r"
+  sh ./"$0" "$directory"
 else
 
   echo "ERROR: Directory does not exits"
